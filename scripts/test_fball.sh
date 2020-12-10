18b676367a0d5095ab879e40a98ba6a89479c068
Ryan Hannigan <astrooo@libra.rhip.utexas.edu>
2020-12-09 23:36:50 -0600

    added code to generate falling ball dataset

---
+++ scripts/test_fball.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python test.py --data_root data --task fball --ckpt WHAT_GOES_HERE
---
+++ scripts/train_fball.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+train.py \
+  --task fball \
+  --data_root data \
+  --q 25 \
+  --Hf 100 \
+  --amort_len 3 \
+  --batch_size 10 \
+  --activation_fn "relu" \
+  --eta 0.0008
